4e68b49f6539a9f081c46eb9bc497b9280966d2f
Jonathan Ferré <jferre@technisys.com>
2018-08-31 10:07:51 -0300

    fix hr

--- js/main.js
+++ js/main.js
@@ -34,9 +34,8 @@ function main() {
         offset: 80
     });
 
-	// Hide nav on click
+
   $(".navbar-nav li a").click(function (event) {
-    // check if window is small enough so dropdown is created
     var toggle = $(".navbar-toggle").is(":visible");
     if (toggle) {
       $(".navbar-collapse").collapse('hide');
